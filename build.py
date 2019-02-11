943745fa42186b28487db827cd8a8137a2f17b2d
marcolcal <marcolcal@gmail.com>
2019-02-11 01:58:12 -0500

    Update build.py

--- build.py
+++ build.py
@@ -1,3 +1,18 @@
+import os
+
+def run(host,cmd):
+    print host, cmd
+    res = os.system('ssh deploy@{} "{}"'.format(host,cmd))
+    if res!=0:
+        raise "Error when running cmd!"
+
+
+def main():
+    print os.environ.get('GIT_BRANCH')
+    if os.environ.get('GIT_BRANCH')=='origin/master':
+        host='production-server.com'
+    else:
+        host='stage-server.com'
         
     run(host, "sudo rm -rf /home/deploy/html")
     os.system('scp -r $WORKSPACE deploy@{}:/home/deploy/html'.format(host))
